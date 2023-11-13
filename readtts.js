3418cde6056dfea0159a85cc47b54da9c7b0575f
alex <alex@youremail>
2023-11-13 21:42:17 +0800

    update

---
+++ readtts.js
@@ -0,0 +1,8 @@
+const testFolder = './ttss/';
+const fs = require('fs');
+
+
+let files = fs.readdirSync(testFolder);
+files=files.reverse().filter(e=>e.indexOf('.mp3')==e.length-4).map(e=> { return {title:e,url:'https://smlog.github.io/data/'+testFolder.substring(1)+e}})
+console.log(files)
+fs.writeFileSync('./ttss.json',JSON.stringify(files));
