e3de6ae062416e97685a9f71342ebfd8e3144421
alex <alex@youremail>
2023-11-14 11:32:47 +0800

    update

--- readtts.js
+++ readtts.js
@@ -2,8 +2,13 @@ const testFolder = './ttss/';
 const fs = require('fs');
 
 
-let files = fs.readdirSync(testFolder);
-files=files.reverse().filter(e=>e.indexOf('.mp3')==e.length-4).map(e=> { return {title:e,url:'https://smlog.github.io/data/'+testFolder.substring(1)+e}})
+let files = fs.readdirSync(testFolder).map(function(v) { 
+                  return { name:v,
+                           time:fs.statSync(testFolder + v).mtime.getTime()
+                         }; 
+               })
+               .sort(function(a, b) { return b.time - a.time; })
+files=files.filter(e=>e.name.indexOf('.mp3')==e.name.length-4).map(e=> { return {title:e.name,date:e.time,url:'https://smlog.github.io/data/'+testFolder.substring(1)+encodeURIComponent(e.name)}})
 console.log(files)
 let output = {content:files}
-fs.writeFileSync('./ttss.json',JSON.stringify(output));
+fs.writeFileSync('./ttss.json',JSON.stringify(output,null,2));
