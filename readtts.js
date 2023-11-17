ac6f21d310e1626f7a849a3bc1bb8786d7fe721b
alex <alex@youremail>
2023-11-17 21:19:44 +0800

    update

--- readtts.js
+++ readtts.js
@@ -9,6 +9,13 @@ let files = fs.readdirSync(testFolder).map(function(v) {
                })
                .sort(function(a, b) { return b.time - a.time; })
 files=files.filter(e=>e.name.indexOf('.mp3')==e.name.length-4).map(e=> { return {title:e.name,date:e.time,url:'https://smlog.github.io/data/'+testFolder.substring(1)+encodeURIComponent(e.name)}})
+files=files.map(e=>{
+		
+		if(fs.existsSync( testFolder+e.title.replace('.mp3','.vtt') )){
+		
+		e['cc']='https://smlog.github.io/data/'+testFolder.substring(1)+encodeURIComponent(e.title.replace('.mp3','.vtt'))
+		}
+		return e;});
 console.log(files)
 let output = {content:files}
 fs.writeFileSync('./ttss.json',JSON.stringify(output,null,2));
